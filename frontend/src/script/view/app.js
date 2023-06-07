6d6aa6a3dd789e22965c36bed289490808ad6e9a
Febri Pujiani <febripujiani.20066@mhs.unesa.ac.id>
2023-06-07 22:44:06 +0700

    mencoba fixing bug routes

--- frontend/src/script/view/app.js
+++ frontend/src/script/view/app.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-trailing-spaces */
 /* eslint-disable import/no-unresolved */
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../../styles/style.css";
@@ -5,6 +6,36 @@ import "bootstrap";
 import routes from "../routes/routes";
 import UrlParser from "../routes/url-parse";
 import DrawerInitiator from "../utils/drawer-initiator";
+
+// class App {
+//   constructor({ button, drawer, content }) {
+//     this._button = button;
+//     this._drawer = drawer;
+//     this._content = content;
+
+//     this._initialAppShell();
+//   }
+
+//   _initialAppShell() {
+//     DrawerInitiator.init({
+//       button: this._button,
+//       drawer: this._drawer,
+//       content: this._content,
+//     });
+//   }
+
+//   async renderPage() {
+//     const url = UrlParser.parseActiveUrlWithCombiner();
+//     const page = routes[url];
+//     this._content.innerHTML = await page.render();
+//     await page.afterRender();
+//     // const skipLinkElem = document.querySelector(".skip-link");
+//     // skipLinkElem.addEventListener("click", (event) => {
+//     //   event.preventDefault();
+//     //   document.querySelector("#mainContent").focus();
+//     // });
+//   }
+// }
 
 class App {
   constructor({ button, drawer, content }) {
@@ -26,8 +57,14 @@ class App {
   async renderPage() {
     const url = UrlParser.parseActiveUrlWithCombiner();
     const page = routes[url];
-    this._content.innerHTML = await page.render();
-    await page.afterRender();
+
+    if (page) {
+      this._content.innerHTML = await page.render();
+      await page.afterRender();
+    } else {
+      this._content.innerHTML = "<p>404 - Page not found</p>";
+    }
+
     // const skipLinkElem = document.querySelector(".skip-link");
     // skipLinkElem.addEventListener("click", (event) => {
     //   event.preventDefault();
